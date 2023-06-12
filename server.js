9c360f36d3b865e2559e02eee20f4b2e8ea80589
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-06-12 15:48:13 -0300

    allowing express port 0.0.0.0 and enviroment varible inject automatically by railway

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
